c78562bf28bc127e7840a84e8241a21ee32656ed
Daniil Karzanov <d.carzanov@gmail.com>
2025-01-29 16:56:52 +0400

    Unshift_3060

--- Push_3057.ts
+++ Push_3057.ts
@@ -1,3 +1,3 @@
-type Result = Push<[1, 2], '3'> // [1, 2, '3']
+// type Result = Push<[1, 2], '3'> // [1, 2, '3']
 
 type Push<A extends Array<any>, V> = A extends [...infer Values] ? [...Values, V] : never 
